3e09168abfe439d0cf083e7b784d4bff417f845c
Afzal <afzalansarip3@gmail.com>
2025-05-03 22:56:53 +0530

    afzal

--- talk_with_youtube.py
+++ talk_with_youtube.py
@@ -8,18 +8,14 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 
-
-# Initialize Embedding Model
 embed = HuggingFaceEmbeddings(
     model_name="sentence-transformers/all-MiniLM-L6-v2",
     model_kwargs={'device': 'cpu'},
     encode_kwargs={'normalize_embeddings': False}
 )
 
-# Initialize Output Parser
 parser = StrOutputParser()
 
-# Define Prompt Template
 prompt = ChatPromptTemplate.from_template(
     "Use the provided context to answer the query.\n"
     "If unsure, state that you don't know. Be concise and factual.\n"
@@ -28,30 +24,28 @@ prompt = ChatPromptTemplate.from_template(
     "Answer:"
 )
 
-# Initialize LLM
+
 llm = ChatGroq(api_key="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",model="llama-3.3-70b-versatile")
 
-# Streamlit App Title
 st.title("🎥 Chat with a YouTube Video")
 st.image(r"C:\Users\99ash\Downloads\unnamed.jpg",width=300)
 
-# User input for YouTube URL
 yt_url = st.text_input("Enter YouTube video URL:")
 
 # Function to Load and Process Video
 def load_video_data(url):
     try:
-        # Try loading English transcript first
+        
         loader = YoutubeLoader.from_youtube_url(
             url,
             add_video_info=False,
-            language=["en"],       # Try English first
-            translation="en"       # Allow translation if needed
+            language=["en"],       
+            translation="en"       
         )
         docs = loader.load()
     except Exception as e:
         st.warning("English subtitles not available. Trying Hindi subtitles with translation...")
-        # Try Hindi if English is not available
+        
         loader = YoutubeLoader.from_youtube_url(
             url,
             add_video_info=False,
@@ -77,7 +71,6 @@ def load_video_data(url):
     )
     return chain
 
-# Main Streamlit App Logic
 if yt_url:
     try:
         chain = load_video_data(yt_url)
